d06a36049fa04debca191befb2de8bf3c7b2c754
Tiago Ávila <tiago.avila09@gmail.com>
2025-01-05 17:49:44 -0300

    day12 part 2 wip

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -5,9 +5,45 @@ use std::{
 };
 
 use glam::IVec2;
-use guid_create::GUID;
 
 const DIRECTIONS: [IVec2; 4] = [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y];
+const DIRECTIONS_MATRIX: [MatrixCell; 4] = [
+    MatrixCell::TOP,
+    MatrixCell::BOTTOM,
+    MatrixCell::RIGHT,
+    MatrixCell::LEFT,
+];
+const DIRECTIONS_CLOCKWISE: [IVec2; 4] = [IVec2::X, IVec2::Y, IVec2::NEG_X, IVec2::NEG_Y];
+
+#[derive(Eq, PartialEq, Hash, Copy, Clone)]
+struct MatrixCell {
+    pub row: isize,
+    pub col: isize,
+}
+
+use std::ops::Add;
+
+impl Add for MatrixCell {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            row: self.row + other.row,
+            col: self.col + other.col,
+        }
+    }
+}
+
+impl MatrixCell {
+    pub fn new(row: isize, col: isize) -> Self {
+        Self { row, col }
+    }
+
+    pub const TOP: Self = Self { row: -1, col: 0 };
+    pub const BOTTOM: Self = Self { row: 1, col: 0 };
+    pub const LEFT: Self = Self { row: 0, col: -1 };
+    pub const RIGHT: Self = Self { row: 0, col: 1 };
+}
 
 fn main() {
     println!("Advent of Code 2024 - day12");
@@ -22,6 +58,125 @@ fn part1(file_path: &str) -> usize {
         .iter()
         .map(|(area, perimeter)| area * perimeter)
         .sum()
+}
+
+fn part2(file_path: &str) -> i32 {
+    let lines = read_file(file_path).unwrap();
+    let mut map = HashMap::new();
+    let rows = lines.len();
+    let cols = lines[0].len();
+
+    for (row_index, line) in lines.iter().enumerate() {
+        for (col_index, c) in line.chars().enumerate() {
+            map.insert(MatrixCell::new(row_index as isize, col_index as isize), c);
+        }
+    }
+
+    let areas = get_areas(&map, &rows, &cols);
+    let areas_sides: Vec<(usize, usize)> = areas
+        .iter()
+        .map(|area| {
+            if area.len() == 1 {
+                return (1, 4);
+            }
+
+            let mut sides_count = 1;
+            let first_position = area[0];
+            let mut current_position = first_position;
+            let mut direction = MatrixCell::RIGHT;
+
+            loop {
+                if first_position == current_position && direction == MatrixCell::TOP {
+                    break;
+                }
+
+                if is_cell_in_border(&current_position, &direction, &area) {
+                    current_position = current_position + direction;
+                } else {
+                    sides_count += 1;
+                    direction = match direction {
+                        MatrixCell::RIGHT => {
+                            let right_neighbor = current_position + MatrixCell::RIGHT;
+                            let top_neighbor = current_position + MatrixCell::TOP;
+                            if !area.contains(&right_neighbor) && !area.contains(&top_neighbor) {
+                                MatrixCell::BOTTOM
+                            } else {
+                                current_position = current_position + MatrixCell::TOP;
+                                MatrixCell::TOP
+                            }
+                        }
+                        MatrixCell::BOTTOM => {
+                            let bottom_neighbor = current_position + MatrixCell::BOTTOM;
+                            let right_neighbor = current_position + MatrixCell::RIGHT;
+                            if !area.contains(&bottom_neighbor) && !area.contains(&right_neighbor) {
+                                MatrixCell::LEFT
+                            } else {
+                                current_position = current_position + MatrixCell::RIGHT;
+                                MatrixCell::RIGHT
+                            }
+                        }
+                        MatrixCell::LEFT => {
+                            let left_neighbor = current_position + MatrixCell::LEFT;
+                            let bottom_neighbor = current_position + MatrixCell::BOTTOM;
+                            if !area.contains(&left_neighbor) && !area.contains(&bottom_neighbor) {
+                                MatrixCell::TOP
+                            } else {
+                                current_position = current_position + MatrixCell::BOTTOM;
+                                MatrixCell::BOTTOM
+                            }
+                        }
+                        MatrixCell::TOP => {
+                            let top_neighbor = current_position + MatrixCell::TOP;
+                            let left_neighbor = current_position + MatrixCell::LEFT;
+                            if !area.contains(&top_neighbor) && !area.contains(&left_neighbor) {
+                                MatrixCell::RIGHT
+                            } else {
+                                current_position = current_position + MatrixCell::LEFT;
+                                MatrixCell::LEFT
+                            }
+                        }
+                        _ => MatrixCell::RIGHT,
+                    };
+                }
+            }
+
+            (area.len(), sides_count)
+        })
+        .collect();
+
+    areas_sides.iter().fold(0, |acc, (area_len, sides_count)| {
+        acc + area_len * sides_count
+    }) as i32
+}
+
+fn is_cell_in_border(
+    position: &MatrixCell,
+    direction: &MatrixCell,
+    area: &Vec<MatrixCell>,
+) -> bool {
+    match *direction {
+        MatrixCell::RIGHT => {
+            let top_neighbor = *position + MatrixCell::TOP;
+            let right_neighbor = *position + MatrixCell::RIGHT;
+            return !area.contains(&top_neighbor) && area.contains(&right_neighbor);
+        }
+        MatrixCell::BOTTOM => {
+            let right_neighbor = *position + MatrixCell::RIGHT;
+            let bottom_neighbor = *position + MatrixCell::BOTTOM;
+            return !area.contains(&right_neighbor) && area.contains(&bottom_neighbor);
+        }
+        MatrixCell::LEFT => {
+            let bottom_neighbor = *position + MatrixCell::BOTTOM;
+            let left_neighbor = *position + MatrixCell::LEFT;
+            return !area.contains(&bottom_neighbor) && area.contains(&left_neighbor);
+        }
+        MatrixCell::TOP => {
+            let left_neighbor = *position + MatrixCell::LEFT;
+            let top_neighbor = *position + MatrixCell::TOP;
+            return !area.contains(&left_neighbor) && area.contains(&top_neighbor);
+        }
+        _ => false,
+    }
 }
 
 fn find_area_and_perimeter(
@@ -52,6 +207,7 @@ fn find_area_and_perimeter(
             while !queue.is_empty() {
                 let current_position = queue.pop().unwrap();
                 let mut current_perimeter = 4;
+
                 if visited.contains(&current_position) {
                     continue;
                 }
@@ -62,7 +218,7 @@ fn find_area_and_perimeter(
                 DIRECTIONS.iter().for_each(|dir| {
                     let neighbor = current_position + *dir;
                     // Add the neighbor to the queue if it is inside the bounds and it has not been visited
-                    if is_inside_bounds(rows, cols, neighbor) && !visited.contains(&neighbor) {
+                    if is_inside_bounds(rows, cols, &neighbor) && !visited.contains(&neighbor) {
                         let neighbor_value = map.get(&neighbor).unwrap();
                         if *neighbor_value == area_identifier {
                             queue.push(neighbor);
@@ -70,7 +226,7 @@ fn find_area_and_perimeter(
                     }
 
                     // Every time a neighbor of the same area is found decrease the parameter of the current area by 1
-                    if is_inside_bounds(rows, cols, neighbor) {
+                    if is_inside_bounds(rows, cols, &neighbor) {
                         let neighbor_value = map.get(&neighbor).unwrap();
                         if *neighbor_value == area_identifier {
                             current_perimeter -= 1;
@@ -91,12 +247,67 @@ fn find_area_and_perimeter(
     areas_and_perimeters
 }
 
-fn is_inside_bounds(rows: &usize, cols: &usize, position: IVec2) -> bool {
+fn get_areas(map: &HashMap<MatrixCell, char>, rows: &usize, cols: &usize) -> Vec<Vec<MatrixCell>> {
+    let mut areas: Vec<Vec<MatrixCell>> = Vec::new();
+    let mut visited: HashSet<MatrixCell> = HashSet::new();
+    let mut area_identifier = ' ';
+    let mut area: Vec<MatrixCell> = Vec::new();
+    let mut queue: Vec<MatrixCell> = Vec::new();
+
+    for row in 0..*rows {
+        for col in 0..*cols {
+            let current_position = MatrixCell::new(row as isize, col as isize);
+
+            if visited.contains(&current_position) {
+                continue;
+            }
+
+            if area_identifier == ' ' {
+                area_identifier = map.get(&current_position).unwrap().clone();
+                queue.push(*&current_position);
+            }
+
+            while !queue.is_empty() {
+                let current_position = queue.pop().unwrap();
+
+                if visited.contains(&current_position) {
+                    continue;
+                }
+
+                visited.insert(current_position);
+                area.push(current_position);
+
+                DIRECTIONS_MATRIX.iter().for_each(|dir| {
+                    let neighbor = current_position + *dir;
+                    // Add the neighbor to the queue if it is inside the bounds and it has not been visited
+                    if is_cell_inside_bounds(rows, cols, &neighbor) && !visited.contains(&neighbor)
+                    {
+                        let neighbor_value = map.get(&neighbor).unwrap();
+                        if *neighbor_value == area_identifier {
+                            queue.push(neighbor);
+                        }
+                    }
+                });
+            }
+
+            areas.push(area);
+            area_identifier = ' ';
+            area = Vec::new();
+        }
+    }
+
+    areas
+}
+
+fn is_inside_bounds(rows: &usize, cols: &usize, position: &IVec2) -> bool {
     position.x >= 0 && position.x < *rows as i32 && position.y >= 0 && position.y < *cols as i32
 }
 
-fn part2(file_path: &str) -> i32 {
-    0
+fn is_cell_inside_bounds(rows: &usize, cols: &usize, position: &MatrixCell) -> bool {
+    position.row >= 0
+        && position.row < *rows as isize
+        && position.col >= 0
+        && position.col < *cols as isize
 }
 
 fn read_file(file_path: &str) -> io::Result<Vec<String>> {
@@ -143,7 +354,32 @@ mod tests {
     }
 
     #[test]
+    fn test_part2_simple_example() {
+        assert_eq!(part2("simple_example_pt2.txt"), 32);
+    }
+
+    #[test]
     fn test_part2() {
-        assert_eq!(part2("test.txt"), 0);
+        assert_eq!(part2("test.txt"), 1206);
+    }
+
+    #[test]
+    fn test_example1_part2() {
+        assert_eq!(part2("example1.txt"), 80);
+    }
+
+    #[test]
+    fn test_example_e_shape_part2() {
+        assert_eq!(part2("e_shape_example.txt"), 236);
+    }
+
+    #[test]
+    fn test_example_2_part2() {
+        assert_eq!(part2("example2.txt"), 436);
+    }
+
+    #[test]
+    fn test_example_3_part2() {
+        assert_eq!(part2("example3.txt"), 368);
     }
 }
