1216a603e81de77125f7698d9a72fb1174384278
Tiago Ávila <tiago.avila09@gmail.com>
2025-01-13 20:38:46 -0300

    day13 part1 passing tests

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -4,6 +4,8 @@ use std::{
     io::{self, BufRead},
 };
 
+use regex::Regex;
+
 pub mod matrix_operations;
 
 fn main() {
@@ -12,8 +14,47 @@ fn main() {
     println!("Part 2: {}", part2("challenge.txt"));
 }
 
-fn part1(file_path: &str) -> i32 {
-    0
+fn part1(file_path: &str) -> usize {
+    let mut lines = read_file(file_path).unwrap();
+    lines.retain(|line| !line.is_empty());
+
+    let re = Regex::new(r"^.+X[+=](\d+).+Y[+=](\d+)").unwrap();
+
+    let chunks: Vec<Option<(usize, usize)>> = lines
+        .chunks(3) // Borrow chunks of 3 elements
+        .map(|chunk| {
+            let (x_values, y_values): (Vec<f64>, Vec<f64>) = chunk
+                .into_iter()
+                .map(|line| {
+                    if let Some(captures) = re.captures(line) {
+                        if let (Some(x), Some(y)) = (captures.get(1), captures.get(2)) {
+                            // Extract and parse the captured groups
+                            let x_value: f64 = x.as_str().parse().unwrap();
+                            let y_value: f64 = y.as_str().parse().unwrap();
+
+                            return (x_value, y_value);
+                        }
+                    }
+
+                    return (0.0, 0.0);
+                })
+                .collect::<Vec<(f64, f64)>>()
+                .into_iter()
+                .unzip();
+
+            let mut matrix = vec![x_values, y_values];
+            crate::matrix_operations::gaussian_elimination(&mut matrix)
+        }) // Convert borrowed slices into owned Vec<String>
+        .collect();
+
+    chunks
+        .into_iter()
+        .filter(|line| line.is_some())
+        .map(|line| {
+            let (a, b) = line.unwrap();
+            a * 3 + b
+        })
+        .sum::<usize>()
 }
 
 fn part2(file_path: &str) -> i32 {
@@ -36,7 +77,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1("test.txt"), 0);
+        assert_eq!(part1("test.txt"), 480);
     }
 
     #[test]
@@ -44,4 +85,3 @@ mod tests {
         assert_eq!(part2("test.txt"), 0);
     }
 }
-
