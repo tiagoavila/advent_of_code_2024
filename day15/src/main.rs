d8136b6867753f805ead375d48921b503c541255
Tiago Ávila <tiago.avila09@gmail.com>
2025-02-07 19:06:36 -0300

    day15 part2 WIP

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -13,22 +13,8 @@ fn main() {
 }
 
 fn part1(file_path: &str) -> i32 {
-    let lines = read_file(file_path).unwrap();
-    let empty_line_index = lines.iter().position(|line| line.is_empty()).unwrap();
-    let warehouse_map = lines[..empty_line_index].to_vec();
-    let movements = lines[empty_line_index + 1..].to_vec().join("");
-
-    let mut robot = matrix::Robot::new(0, 0);
-    let mut map: HashMap<(usize, usize), char> = HashMap::new();
-    for (row, line) in warehouse_map.iter().enumerate() {
-        for (col, ch) in line.chars().enumerate() {
-            map.insert((row, col), ch);
-
-            if ch == '@' {
-                robot = matrix::Robot::new(row, col);
-            }
-        }
-    }
+    let (warehouse, movements) = get_warehouse_and_movements(file_path);
+    let (mut robot, mut warehouse_map) = get_warehouse_map(warehouse);
 
     for movement in movements.chars() {
         let direction = match movement {
@@ -38,10 +24,11 @@ fn part1(file_path: &str) -> i32 {
             '>' => matrix::Direction::RIGHT,
             _ => panic!("Invalid movement"),
         };
-        robot.move_robot(&mut map, direction);
+        robot.move_robot(&mut warehouse_map, direction);
     }
 
-    map.iter()
+    warehouse_map
+        .iter()
         .filter_map(|position| {
             if position.1 == &'O' {
                 Some((position.0 .0, position.0 .1))
@@ -55,7 +42,34 @@ fn part1(file_path: &str) -> i32 {
 }
 
 fn part2(file_path: &str) -> i32 {
-    let mut lines = read_file(file_path).unwrap();
+    let (warehouse, movements) = get_warehouse_and_movements(file_path);
+
+    let doubled_warehouse: Vec<String> = warehouse.iter().map(|line| {
+        line.chars().map(|ch| {
+            match ch {
+                '#' => "##",
+                'O' => "[]",
+                '.' => "..",
+                '@' => "@.",
+                _ => "",  // Using _ is the conventional way to match any remaining patterns
+            }.to_string()
+        }).collect::<String>()
+    }).collect();
+
+    let (mut robot, mut warehouse_map) = get_warehouse_map(doubled_warehouse);
+
+    for movement in movements.chars() {
+        let direction = match movement {
+            '^' => matrix::Direction::TOP,
+            'v' => matrix::Direction::BOTTOM,
+            '<' => matrix::Direction::LEFT,
+            '>' => matrix::Direction::RIGHT,
+            _ => panic!("Invalid movement"),
+        };
+        robot.move_robot(&mut warehouse_map, direction);
+    }
+
+    let (mut robot, mut warehouse_map) = get_warehouse_map(warehouse);
     0
 }
 
@@ -67,6 +81,29 @@ fn read_file(file_path: &str) -> io::Result<Vec<String>> {
     // Collect the lines into a vector
     let lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
     Ok(lines)
+}
+
+fn get_warehouse_map(warehouse: Vec<String>) -> (matrix::Robot, HashMap<(usize, usize), char>) {
+    let mut robot = matrix::Robot::new(0, 0);
+    let mut warehouse_map: HashMap<(usize, usize), char> = HashMap::new();
+    for (row, line) in warehouse.iter().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            warehouse_map.insert((row, col), ch);
+
+            if ch == '@' {
+                robot = matrix::Robot::new(row, col);
+            }
+        }
+    }
+    (robot, warehouse_map)
+}
+
+fn get_warehouse_and_movements(file_path: &str) -> (Vec<String>, String) {
+    let lines = read_file(file_path).unwrap();
+    let empty_line_index = lines.iter().position(|line| line.is_empty()).unwrap();
+    let warehouse = lines[..empty_line_index].to_vec();
+    let movements = lines[empty_line_index + 1..].to_vec().join("");
+    (warehouse, movements)
 }
 
 #[cfg(test)]
@@ -87,4 +124,10 @@ mod tests {
     fn test_part2() {
         assert_eq!(part2("test.txt"), 0);
     }
+
+    #[test]
+    fn test_simple_example_part2() {
+        assert_eq!(part2("simple_example2.txt"), 2028);
+    }
+
 }
--- day15/src/matrix.rs
+++ day15/src/matrix.rs
@@ -4,7 +4,6 @@ use std::collections::HashMap;
 pub struct Robot {
     pub row: usize,
     pub col: usize,
-    pub is_stuck: bool,
 }
 
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
@@ -15,11 +14,7 @@ pub struct Direction {
 
 impl Robot {
     pub fn new(row: usize, col: usize) -> Self {
-        Self {
-            row,
-            col,
-            is_stuck: false,
-        }
+        Self { row, col }
     }
 
     pub fn move_robot(
@@ -30,7 +25,6 @@ impl Robot {
         let new_robot_position = Self {
             row: (self.row as isize + direction.row) as usize,
             col: (self.col as isize + direction.col) as usize,
-            is_stuck: self.is_stuck,
         };
 
         let new_position_value = warehouse_map
@@ -45,22 +39,20 @@ impl Robot {
                     .and_modify(|v| *v = '.');
                 *self = new_robot_position;
             }
-            '#' => {
-                self.is_stuck = true;
-            }
             'O' => {
                 let mut empty_or_wall_position = Self {
                     row: (new_robot_position.row as isize + direction.row) as usize,
                     col: (new_robot_position.col as isize + direction.col) as usize,
-                    is_stuck: false,
                 };
                 while warehouse_map
                     .get(&(empty_or_wall_position.row, empty_or_wall_position.col))
                     .unwrap()
                     == &'O'
                 {
-                    empty_or_wall_position.row = (empty_or_wall_position.row as isize + direction.row) as usize;
-                    empty_or_wall_position.col = (empty_or_wall_position.col as isize + direction.col) as usize;
+                    empty_or_wall_position.row =
+                        (empty_or_wall_position.row as isize + direction.row) as usize;
+                    empty_or_wall_position.col =
+                        (empty_or_wall_position.col as isize + direction.col) as usize;
                 }
 
                 match warehouse_map
@@ -78,12 +70,72 @@ impl Robot {
 
                         *self = new_robot_position;
                     }
-                    '#' => {
-                        self.is_stuck = true;
-                    }
                     _ => (),
                 }
             }
+            _ => (),
+        }
+
+        *self
+    }
+
+    pub fn move_robot_part2(
+        &mut self,
+        warehouse_map: &mut HashMap<(usize, usize), char>,
+        direction: Direction,
+    ) -> Self {
+        let new_robot_position = Self {
+            row: (self.row as isize + direction.row) as usize,
+            col: (self.col as isize + direction.col) as usize,
+        };
+
+        let new_position_value = warehouse_map
+            .get_mut(&(new_robot_position.row, new_robot_position.col))
+            .unwrap();
+
+        match *new_position_value {
+            '.' => {
+                *new_position_value = '@';
+                warehouse_map
+                    .entry((self.row, self.col))
+                    .and_modify(|v| *v = '.');
+                *self = new_robot_position;
+            }
+            '[' | ']' => match direction {
+                Direction::LEFT | Direction::RIGHT => {
+                    let mut empty_or_wall_position = Self {
+                        row: (new_robot_position.row as isize + direction.row) as usize,
+                        col: (new_robot_position.col as isize + direction.col) as usize,
+                    };
+
+                    let mut positions_to_move_stack: Vec<(usize, usize)> = Vec::new();
+                    positions_to_move_stack.push((empty_or_wall_position.row, empty_or_wall_position.col));
+
+                    while let Some(&ch) = warehouse_map.get(&(empty_or_wall_position.row, empty_or_wall_position.col)) {
+                        if ch == '[' || ch == ']' {
+                            empty_or_wall_position.row =
+                                (empty_or_wall_position.row as isize + direction.row) as usize;
+                            empty_or_wall_position.col =
+                                (empty_or_wall_position.col as isize + direction.col) as usize;
+                            positions_to_move_stack.push((empty_or_wall_position.row, empty_or_wall_position.col));
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if let Some(ch) = warehouse_map.get_mut(&(empty_or_wall_position.row, empty_or_wall_position.col)) {
+                        if *ch == '.' {
+                            while !positions_to_move_stack.is_empty() {
+                                let (row, col) = positions_to_move_stack.pop().unwrap();
+                                let (previous_row, previous_col) = positions_to_move_stack.last().unwrap();
+                                *warehouse_map.get_mut(&(row, col)).unwrap() = *warehouse_map.get(&(*previous_row, *previous_col)).unwrap();
+                            }
+                        }
+                    }
+                }
+                Direction::TOP | Direction::BOTTOM => {}
+                _ => (),
+            },
             _ => (),
         }
 
