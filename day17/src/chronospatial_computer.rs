390b4bf0b5dea7fdd0307c2e2f24465e955b471a
Tiago Ávila <tiago.avila09@gmail.com>
2025-03-14 20:36:22 -0300

    day 17 part 1

---
+++ day17/src/chronospatial_computer.rs
@@ -0,0 +1,159 @@
+pub struct Computer {
+    pub register_a: usize,
+    pub register_b: usize,
+    pub register_c: usize,
+    instruction_pointer: usize,
+    instructions: Vec<usize>,
+    pub output: Vec<usize>,
+}
+
+impl Computer {
+    pub fn new(
+        register_a: usize,
+        register_b: usize,
+        register_c: usize,
+        instructions: String,
+    ) -> Computer {
+        Computer {
+            register_a,
+            register_b,
+            register_c,
+            instruction_pointer: 0,
+            instructions: instructions
+                .split(',')
+                .map(|c| c.parse::<usize>().unwrap())
+                .collect(),
+            output: Vec::new(),
+        }
+    }
+
+    pub fn new_from_input(input: &mut Vec<String>) -> Computer {
+        let register_a = Self::get_register_from_input(input[0].clone(), "A");
+        let register_b = Self::get_register_from_input(input[1].clone(), "B");
+        let register_c = Self::get_register_from_input(input[2].clone(), "C");
+        let instructions = input[4].replace("Program: ", "");
+
+        Self::new(register_a, register_b, register_c, instructions)
+    }
+
+    pub fn execute(&mut self) {
+        loop {
+            if self.instruction_pointer >= self.instructions.len() {
+                break;
+            }
+
+            let opcode: usize = self.instructions[self.instruction_pointer];
+            let operand: usize = self.instructions[self.instruction_pointer + 1];
+            match opcode {
+                0 => self.adv(operand),
+                1 => self.bxl(operand),
+                2 => self.bst(operand),
+                3 => self.jnz(operand),
+                4 => self.bxc(operand),
+                5 => self.out(operand),
+                6 => self.bdv(operand),
+                7 => self.cdv(operand),
+                _ => self,
+            };
+
+            if opcode != 3 {
+                self.instruction_pointer += 2;
+            }
+        }
+    }
+
+    fn get_register_from_input(input: String, register_name: &str) -> usize {
+        let register = format!("Register {}: ", register_name);
+        input.replacen(&register, "", 1).parse::<usize>().unwrap()
+    }
+
+    /// The adv instruction (opcode 0) performs division.
+    /// The numerator is the value in the A register.
+    /// The denominator is found by raising 2 to the power of the instruction's combo operand.
+    /// (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.
+    /// The result of the division operation is truncated to an integer and then written to the A register.
+    fn adv(&mut self, operand: usize) -> &mut Computer {
+        self.register_a = self.do_division(operand);
+        self
+    }
+
+    /// The bxl instruction (opcode 1) calculates the bitwise XOR of register B and 
+    /// the instruction's literal operand, then stores the result in register B.
+    fn bxl(&mut self, operand: usize) -> &mut Computer {
+        self.register_b = self.register_b ^ operand;
+        self
+    }
+
+    /// The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 
+    /// (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+    fn bst(&mut self, operand: usize) -> &mut Computer {
+        let operand: usize = self.get_combo_operand_value(operand);
+        let modulo_8 = operand % 8;
+        self.register_b = modulo_8;
+        self
+    }
+
+    /// The jnz instruction (opcode 3) does nothing if the A register is 0.
+    /// However, if the A register is not zero, it jumps by setting the instruction pointer
+    /// to the value of its literal operand; if this instruction jumps, the instruction
+    /// pointer is not increased by 2 after this instruction.
+    fn jnz(&mut self, operand: usize) -> &mut Computer {
+        if self.register_a != 0 {
+            self.instruction_pointer = operand;
+        } else {
+            self.instruction_pointer += 2;
+        }
+
+        self
+    }
+
+    /// The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C,
+    /// then stores the result in register B. (For legacy reasons, this instruction reads an 
+    /// operand but ignores it.)
+    fn bxc(&mut self, _: usize) -> &mut Computer {
+        self.register_b = self.register_b ^ self.register_c;
+        self
+    }
+
+    /// The out instruction (opcode 5) calculates the value of its combo operand modulo 8,
+    /// then outputs that value. (If a program outputs multiple values,
+    /// they are separated by commas.)
+    fn out(&mut self, operand: usize) -> &mut Computer {
+        let operand: usize = self.get_combo_operand_value(operand);
+        let modulo_8 = operand % 8;
+        self.output.push(modulo_8);
+        self
+    }
+
+    /// The bdv instruction (opcode 6) works exactly like the adv instruction except that 
+    /// the result is stored in the B register. (The numerator is still read from the A 
+    /// register.)
+    fn bdv(&mut self, operand: usize) -> &mut Computer {
+        self.register_b = self.do_division(operand);
+        self
+    }
+    
+    /// The cdv instruction (opcode 7) works exactly like the adv instruction except that 
+    /// the result is stored in the C register. (The numerator is still read from the A 
+    /// register.)
+    fn cdv(&mut self, operand: usize) -> &mut Computer {
+        self.register_c = self.do_division(operand);
+        self
+    }
+
+    fn get_combo_operand_value(&self, operand: usize) -> usize {
+        match operand {
+            4 => self.register_a,
+            5 => self.register_b,
+            6 => self.register_c,
+            _ => operand,
+        }
+    }
+
+    fn do_division(&mut self, operand: usize) -> usize {
+        let operand: usize = self.get_combo_operand_value(operand);
+        let denominator = (1 << operand) as usize;
+        let division = self.register_a / denominator;
+        division
+    }
+}
---
+++ day17/src/main.rs
@@ -0,0 +1,147 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::{self, BufRead},
+};
+
+use chronospatial_computer::Computer;
+mod chronospatial_computer;
+
+fn main() {
+    println!("Advent of Code 2024 - day17");
+    println!("Part 1: {}", part1("challenge.txt"));
+    println!("Part 2: {}", part2("challenge.txt"));
+}
+
+fn part1(file_path: &str) -> String {
+    let mut lines = read_file(file_path).unwrap();
+    let mut computer: Computer = Computer::new_from_input(&mut lines);
+    computer.execute();
+    computer.output.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(",")
+}
+
+fn part2(file_path: &str) -> i32 {
+    let mut lines = read_file(file_path).unwrap();
+    0
+}
+
+fn read_file(file_path: &str) -> io::Result<Vec<String>> {
+    // Open the file
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+
+    // Collect the lines into a vector
+    let lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
+    Ok(lines)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::chronospatial_computer::Computer;
+
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("test.txt"), "4,6,3,5,6,3,5,2,1,0");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("test.txt"), 0);
+    }
+
+    /// If register C contains 9, the program 2,6 would set register B to 1.
+    #[test]
+    fn test_example_1() {
+        let mut lines: Vec<String> = vec![
+            "Register A: 0",
+            "Register B: 0",
+            "Register C: 9",
+            "",
+            "Program: 2,6",
+        ]
+        .iter()
+        .map(|&line| line.to_string())
+        .collect();
+        let mut computer: Computer = Computer::new_from_input(&mut lines);
+        computer.execute();
+        assert_eq!(computer.register_b, 1);
+    }
+
+    /// If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+    #[test]
+    fn test_example_2() {
+        let mut lines: Vec<String> = vec![
+            "Register A: 10",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 5,0,5,1,5,4",
+        ]
+        .iter()
+        .map(|&line| line.to_string())
+        .collect();
+        let mut computer: Computer = Computer::new_from_input(&mut lines);
+        computer.execute();
+        assert_eq!(computer.output, vec![0, 1, 2]);
+    }
+
+    ///If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+    #[test]
+    fn test_example_3() {
+        let mut lines: Vec<String> = vec![
+            "Register A: 2024",
+            "Register B: 0",
+            "Register C: 0",
+            "",
+            "Program: 0,1,5,4,3,0",
+        ]
+        .iter()
+        .map(|&line| line.to_string())
+        .collect();
+        let mut computer: Computer = Computer::new_from_input(&mut lines);
+        computer.execute();
+        assert_eq!(
+            computer.output,
+            vec![4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]
+        );
+        assert_eq!(computer.register_a, 0);
+    }
+
+    /// If register B contains 29, the program 1,7 would set register B to 26
+    #[test]
+    fn test_example_4() {
+        let mut lines: Vec<String> = vec![
+            "Register A: 0",
+            "Register B: 29",
+            "Register C: 0",
+            "",
+            "Program: 1,7",
+        ]
+        .iter()
+        .map(|&line| line.to_string())
+        .collect();
+        let mut computer: Computer = Computer::new_from_input(&mut lines);
+        computer.execute();
+        assert_eq!(computer.register_b, 26);
+    }
+
+    /// If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+    #[test]
+    fn test_example_5() {
+        let mut lines: Vec<String> = vec![
+            "Register A: 0",
+            "Register B: 2024",
+            "Register C: 43690",
+            "",
+            "Program: 4,0",
+        ]
+        .iter()
+        .map(|&line| line.to_string())
+        .collect();
+        let mut computer: Computer = Computer::new_from_input(&mut lines);
+        computer.execute();
+        assert_eq!(computer.register_b, 44354);
+    }
+}
